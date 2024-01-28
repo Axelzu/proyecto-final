1229272165b0b70fe85e5f68459ae00802e5810c
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2024-01-28 17:44:32 -0500

    primera parte del codigo en donde se utilizara una estructura para determinar el horario de las citas

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Estructura para representar una cita médica
+struct Cita {
+    int dia;
+    int mes;
+    int año;
+    int hora;
+    int minuto;
+    char especialidad[50];
+};
