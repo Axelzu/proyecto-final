4c60d6005c3ae9b882d8c091dbd8e0f20531cda4
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2024-01-28 17:45:25 -0500

    segunda parte donde se agrega una funcion para verificar la cita disponible

--- main.c
+++ main.c
@@ -9,3 +9,16 @@ struct Cita {
     int minuto;
     char especialidad[50];
 };
+//  esta función  sirve para verificar si una cita está disponible
+int citaDisponible(struct Cita citas[], int numCitas, struct Cita nuevaCita) {
+    for (int i = 0; i < numCitas; i++) {
+        if (citas[i].dia == nuevaCita.dia &&
+            citas[i].mes == nuevaCita.mes &&
+            citas[i].año == nuevaCita.año &&
+            citas[i].hora == nuevaCita.hora &&
+            citas[i].minuto == nuevaCita.minuto) {
+            return 0; // La cita ya está reservada
+        }
+    }
+    return 1; // La cita está disponible
+}
